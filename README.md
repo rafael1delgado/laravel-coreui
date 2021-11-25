4f0349c430e2abb3a628b606cecde2a743503998
rafael1delgado <delgadorafael2011@gmail.com>
2021-11-25 12:37:53 -0400

    [add] Agrega los archivos

---
+++ README.md
@@ -0,0 +1,44 @@
+# Laravel Core UI 
+
+Proyecto Laravel 8 con CoreUI 3
+
+![Preview Laravel CoreUI](resources/assets/img/preview.png?raw=true "Laravel CoreUI")
+
+# Instalación
+
+### Instalar las dependencias de PHP
+
+``` bash
+composer install
+```
+
+### Generar el APP_KEY 
+Es importante copiar el .env.example y guardar como .env
+``` bash
+php artisan key:generate
+```
+
+
+### Instalar las dependencias de Node.js
+``` bash
+npm install
+```
+
+### Generar los mixing
+``` bash
+npm run dev
+```
+
+### Levantar el servidor
+``` bash
+php artisan serve
+```
+
+### Visitar la ruta '/'
+
+Ir a ```http://localhost:8000/```
+
+
+# Créditos
+
+* [delgadorafael2011@gmail.com](delgadorafael2011@gmail.com)
---
+++ resources/views/dashboard/shared/nav-builder.blade.php
@@ -0,0 +1,24 @@
+    <div class="c-sidebar-brand">
+        <img class="c-sidebar-brand-full" src="{{ url('/assets/brand/coreui-base-white.svg') }}" width="118" height="46" alt="CoreUI Logo">
+        <img class="c-sidebar-brand-minimized" src="{{ url('assets/brand/coreui-signet-white.svg') }}" width="118" height="46" alt="CoreUI Logo">
+    </div>
+    <ul class="c-sidebar-nav">
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-home"></use>
+                </svg>
+                Dashboard
+            </a>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="#">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-input"></use>
+                </svg>
+                Users
+            </a>
+        </li>
+    </ul>
+    <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
+</div>
